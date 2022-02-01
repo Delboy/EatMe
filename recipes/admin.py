31e3a7f0a39823be9143dbde375eefba04e7a97d
Delboy <Derek.degoey@hotmail.co.uk>
2022-02-01 16:25:25 +0000

    add filter and search to recipes in admin and prepopulated slug field

--- recipes/admin.py
+++ recipes/admin.py
@@ -6,5 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'published_on', 'featured')
+    search_fields = ['title', 'description', 'method', 'ingredients']
+    prepopulated_fields = {'slug': ('author', 'title')}
+    list_filter = ('published_on', 'author', 'featured', 'vegetarian', 'vegan')
     summernote_fields = ('ingredients', 'method')
 
