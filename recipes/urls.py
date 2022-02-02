98a04445347d31aae188840db4eaba4c8bd85c6f
Delboy <Derek.degoey@hotmail.co.uk>
2022-02-02 14:06:49 +0000

    add recipe template with recipe card and pagination navigation, added recipe url

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+     path('recipes/', views.RecipeList.as_view(), name='recipes')
 ]
