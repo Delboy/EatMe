71b8ca83260149062043eef14e6360d5b8c0510f
Delboy <Derek.degoey@hotmail.co.uk>
2022-02-03 14:38:41 +0000

    wired up comment form in views

--- recipes/views.py
+++ recipes/views.py
@@ -39,4 +39,34 @@ class RecipeDetail(View):
                 "comment_form": CommentForm()
             }
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.all()
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
         
