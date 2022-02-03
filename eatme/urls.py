a4b0bd87bc77d5847d6a7abde14e29712b99fece
Delboy <Derek.degoey@hotmail.co.uk>
2022-02-03 10:58:02 +0000

    installed allauth

--- eatme/urls.py
+++ eatme/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('recipes.urls'), name='recipes_urls')
+    path('', include('recipes.urls'), name='recipes_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
