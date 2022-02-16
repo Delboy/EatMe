b058363f8f27580d79fa06639ff08a5ffc02a9f8
Delboy <Derek.degoey@hotmail.co.uk>
2022-02-16 15:29:47 +0000

    removed approved status from comments model

--- recipes/models.py
+++ recipes/models.py
@@ -48,7 +48,6 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['created_on']
