9167ffac3f8a3c4539bf0775829e6da030de1654
Delboy <Derek.degoey@hotmail.co.uk>
2022-03-04 15:47:33 +0000

    add docstrings to views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.views import generic, View
 from django.views.generic import CreateView, UpdateView
 from django.db.models import Count
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
 from .models import Recipe, Comment
 from .forms import CommentForm, RecipeForm
-from django.core.paginator import Paginator
 
 
 class HomeView(generic.ListView):
+    """ Renders the home page"""
     def get(self, request):
+        """What happens for a GET request"""
         recipes = Recipe.objects.filter(featured=True)
-        top_recipes = Recipe.objects.annotate(like_count=Count('likes')).order_by('-like_count')
+        top_recipes = Recipe.objects.annotate(
+            like_count=Count('likes')).order_by('-like_count')
         context = {
             "recipes": recipes,
             "top_recipes": top_recipes
@@ -22,17 +25,20 @@ class HomeView(generic.ListView):
 
 
 class RecipeList(generic.ListView):
+    """Renders all recipes"""
     def get(self, request):
+        """What happens for a GET request"""
         recipes = Recipe.objects.all()
-        paginator = Paginator(recipes, 6) # Show 6 Recipes per page.
+        paginator = Paginator(recipes, 6)  # Show 6 Recipes per page.
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         return render(request, 'recipes.html', {'page_obj': page_obj})
-        
 
 
 class RecipeDetail(View):
-    def get(self, request, slug, *args, **kwargs):
+    """Renders recipe details"""
+    def get(self, request, slug):
+        """What happens for a GET request"""
         queryset = Recipe.objects.all()
         recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.order_by('created_on')
@@ -51,7 +57,8 @@ class RecipeDetail(View):
             }
         )
 
-    def post(self, request, slug, *args, **kwargs):
+    def post(self, request, slug):
+        """What happens for a POST request"""
         queryset = Recipe.objects.all()
         recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.order_by('created_on')
@@ -84,7 +91,9 @@ class RecipeDetail(View):
 
 
 class RecipeLike(View):
+    """Likes/unlikes recipes"""
     def post(self, request, slug):
+        """What happens for a POST request"""
         recipe = get_object_or_404(Recipe, slug=slug)
 
         if recipe.likes.filter(id=request.user.id).exists():
@@ -95,31 +104,33 @@ class RecipeLike(View):
 
 
 class UsersRecipeList(generic.ListView):
+    """Renders users recipes"""
     def get(self, request):
+        """What happens for a GET request"""
         if request.user.is_authenticated:
             recipes = Recipe.objects.filter(author=request.user)
-            paginator = Paginator(recipes, 6) # Show 6 Recipes per page.
+            paginator = Paginator(recipes, 6)  # Show 6 Recipes per page.
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
             return render(request, 'your_recipes.html', {'page_obj': page_obj})
         else:
             return render(request, 'your_recipes.html')
-            
+
 
 class AddRecipe(SuccessMessageMixin, CreateView):
+    """Adds Recipe"""
     model = Recipe
     form_class = RecipeForm
     template_name = 'add_recipe.html'
     success_message = 'Recipe Successfully Added'
+
     def form_valid(self, form):
         form.instance.author = self.request.user
-        
         return super().form_valid(form)
-    
-    
 
 
 class EditRecipe(SuccessMessageMixin, UpdateView):
+    """Edits recipe"""
     model = Recipe
     template_name = 'edit_recipe.html'
     form_class = RecipeForm
@@ -127,6 +138,7 @@ class EditRecipe(SuccessMessageMixin, UpdateView):
 
 
 def delete_recipe(request, pk):
+    """Deletes recipe"""
     recipe = get_object_or_404(Recipe, id=pk)
     recipe.delete()
     messages.success(request, 'Recipe Deleted Successfully')
@@ -134,6 +146,7 @@ def delete_recipe(request, pk):
 
 
 class EditComment(SuccessMessageMixin, UpdateView):
+    """Edits comment"""
     model = Comment
     template_name = 'edit_comment.html'
     form_class = CommentForm
@@ -141,36 +154,48 @@ class EditComment(SuccessMessageMixin, UpdateView):
 
 
 def delete_comment(request, pk):
+    """Deletes comment"""
     comment = get_object_or_404(Comment, id=pk)
     comment.delete()
     messages.success(request, 'Comment Deleted Successfully')
-    return HttpResponseRedirect(reverse('recipe_detail', args=[comment.recipe.slug]))
-    
+    return HttpResponseRedirect(reverse(
+        'recipe_detail', args=[comment.recipe.slug]))
 
-    
+
 class UsersFavRecipes(generic.ListView):
+    """Renders users liked recipes"""
     def get(self, request):
+        """What happens for a GET request"""
         if request.user.is_authenticated:
             recipes = Recipe.objects.filter(likes=request.user.id)
-            paginator = Paginator(recipes, 6) # Show 6 Recipes per page.
+            paginator = Paginator(recipes, 6)  # Show 6 Recipes per page.
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
-            return render(request, 'favourite_recipes.html', {'page_obj': page_obj})
+            return render(request, 'favourite_recipes.html', {
+                'page_obj': page_obj
+                })
         else:
             return render(request, 'favourite_recipes.html')
 
 
 def search_recipes(request):
+    """Renders search results"""
     if request.method == "POST":
         searched = request.POST.get('searched')
         if searched.lower() == 'vegan':
             recipes = Recipe.objects.filter(vegan=True)
-            return render(request, 'search_recipes.html', {'searched': searched, 'recipes':recipes})
+            return render(request, 'search_recipes.html', {
+                'searched': searched, 'recipes': recipes
+                })
         elif searched.lower() == 'vegetarian':
             recipes = Recipe.objects.filter(vegetarian=True)
-            return render(request, 'search_recipes.html', {'searched': searched, 'recipes':recipes})
+            return render(request, 'search_recipes.html', {
+                'searched': searched, 'recipes': recipes
+                })
         else:
             recipes = Recipe.objects.filter(title__icontains=searched)
-            return render(request, 'search_recipes.html', {'searched': searched, 'recipes':recipes})
+            return render(request, 'search_recipes.html', {
+                'searched': searched, 'recipes': recipes
+                })
     else:
         return render(request, 'search_recipes.html')
