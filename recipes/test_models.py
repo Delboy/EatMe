cf4e1ea815398fb67a02438083e42967109e2975
Delboy <Derek.degoey@hotmail.co.uk>
2022-03-11 12:25:56 +0000

    add test for calling strings, deleted test that I accidently saved in test_models

--- recipes/test_models.py
+++ recipes/test_models.py
@@ -1,9 +1,26 @@
 from django.test import TestCase
-from .models import Recipe, Comment
+from .models import Comment, Recipe
 
 
 class TestModels(TestCase):
-    
-    def test_featured_defaults_to_false(self):
-        recipe = Recipe.objects.create(title='Test')
-        self.assertFalse(recipe.featured)
+    """
+    Testing for models
+    """
+    def test_recipe_model_str(self):
+        """
+        Testing that calling the string method returns correct string
+        """
+        recipe = Recipe.objects.create(title='Test Recipe')
+        self.assertEqual(str(recipe), 'Test Recipe')
+
+    def test_comment_model_str(self):
+        """
+        Testing that calling the string method returns correct string
+        """
+        recipe = Recipe.objects.create(title='Test Recipe')
+        comment = Comment.objects.create(
+            body='Test',
+            recipe=recipe, 
+            name='Test_user'
+            )
+        self.assertEqual(str(comment), 'Comment Test by Test_user')
