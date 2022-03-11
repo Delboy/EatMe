9defecbbb5637ce4c5ef9114ae36df160fffd44c
Delboy <Derek.degoey@hotmail.co.uk>
2022-03-11 14:34:51 +0000

    fixed pylint errors for app.py

--- recipes/apps.py
+++ recipes/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class RecipesConfig(AppConfig):
+    """Config for Recipe App"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
